0b2d1134e5288cf764193a3f2e45a3c34e59d6d9
Shikha <sj023279@gmail.com>
2021-10-09 18:53:41 +0530

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!------------------------------------------------------------------------------------ -->
+    <script src="src/js/jquery.min.js"></script>
+    <script src="src/js/popper.min.js"></script>
+    <script src="src/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/8aa71aba32.js " crossorigin="anonymous"></script>
+<!------------------------------------------------------------------------------------ -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,50 @@
+<?php include 'header.php';?>
+
+<main>
+
+    <section class="soon">
+        <div class="container">
+            <div class="row">
+                <div class="soon-inner">
+                    <a href="https://mitoqindia.com/en" class="header-logo">
+                        <img src="src/images/logo.gif" alt="" class="logo">
+                    </a><br>
+                    <div class="soon-text">
+                        <h1 class="heading">WE'RE BUILDING OUR WEBSITE</h1>
+                        <P>Sign up now and be the first one to know when its ready!</P>
+                        <form action="">
+                            <div class="input-group mb-3">
+                                <input type="email" class="form-control" placeholder="Enter Your Email">
+                                <div class="input-group-prepend">
+                                    <button class="btn btn-1" type="button"><img src="src/images/arrow.png" alt=""></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="social-links">
+                        <ul>
+                            <li>
+                                <a href=""><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                            </li>
+                            <li>
+                                <a href=""><i class="fab fa-youtube" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="copyright">
+                        <p> Copyright © <a href="https://mitoqindia.com/en">MitoQIndia</a> 2021. All rights reserved. Designed &amp; Developed by<br><a href="https://ebslon.com/" target="blank">Ebslon Infotech</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>    
+
+</main>
+
+<?php include 'footer.php';?>
